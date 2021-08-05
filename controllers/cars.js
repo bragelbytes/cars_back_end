bfb010fe93b161ee7a078fc43328402fa7b56e23
jeffbat <75343055+jeffbat@users.noreply.github.com>
2021-08-05 17:55:32 -0400

    server

--- controllers/cars.js
+++ controllers/cars.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const router = express.Router()
-const Cars = require("..models/cars.js")
+const Cars = require("../models/cars.js")
 
 //GET
 router.get("/", (req, res) => {
